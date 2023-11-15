98c4b6db409327db9843f43f48a48d4dcb4c53f4
leonf <ldavis@unsa.edu.pe>
2023-11-15 11:57:40 -0500

    ac

--- src/test/java/testCalculator.java
+++ src/test/java/testCalculator.java
@@ -1,3 +1,4 @@
+
 import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -50,17 +51,7 @@ public class testCalculator {
         assertEquals("5", result);
     }
 
-    @Test
-    public void testError() {
-
-        driver.findElement(By.id("cpar1")).sendKeys("20");
-        driver.findElement(By.id("cpar2")).sendKeys("100");
-
-        driver.findElement(By.xpath("//*[@id=\"content\"]/form[1]/table/tbody/tr[2]/td/input[2]")).click();
-
-        String result = driver.findElement(By.xpath("//*[@id=\"content\"]/p[2]/font/b")).getText();
-        assertEquals("-2", result);
-    }
+    
 
     @Test
     public void test2() {
@@ -74,5 +65,20 @@ public class testCalculator {
         assertEquals("10", result);
     }
 
-    
+    @Test
+    public void test3() {
+        //este caso inserto letras en un campo numerico y genera error
+        driver.findElement(By.id("cpar1")).sendKeys("ab");
+        driver.findElement(By.id("cpar2")).sendKeys("100");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/form[1]/table/tbody/tr[2]/td/input[2]")).click();
+        try {
+            String result = driver.findElement(By.xpath("//*[@id=\"content\"]/p[2]/font/b")).getText();
+            assertEquals("10", result);
+        } catch (Exception e) {
+            String result = driver.findElement(By.xpath("//*[@id=\"content\"]/p[2]/font")).getText();
+            assertEquals("Please provide two numeric values in any fields below.", result);
+        }
+        
+    }
+
 }
